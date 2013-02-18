1262ccff42dfda075ac8c5aac84ea71962254590
Josh Valdez <exxonvaldeez@gmail.com>
2013-02-18 15:58:51 -0800

    Modified client

--- node/app.js
+++ node/app.js
@@ -6,14 +6,16 @@ var io = require('socket.io').listen(server);
 var parse = require('./server/c-parse.js').importFeed(process.argv[2], io);
 
 // Config
-app.configure(function(){
-    app.use('', express.static(__dirname + '/public'));
-});
+app.configure(function()
+    {
+        app.use('', express.static(__dirname + '/public'));
+    });
 
 // Views
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/client/index.html');
-});
+app.get('/', function (req, res)
+    {
+        res.sendfile(__dirname + '/client/index.html');
+    });
 
 // Server
 server.listen(8080);
