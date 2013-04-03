52f9876b20e7451d18ca23d7d3c8a6ca929f59f8
Danny <danny@spin.(none)>
2013-04-02 23:15:15 -0700

    rewrote isotope directive for improved functionality.

--- node/public/js/c-ssIsoGrid.js
+++ node/public/js/c-ssIsoGrid.js
@@ -4,12 +4,11 @@ myModule.directive('ssIsoGrid', function() {
 			itemSelector : '.iso-item',
 			getSortData : {
 				title : function(item) {
-					//console.log(item.find('.song-title').text());
 					return item.find('.song-title').text();
 				}	
 			}
 		});
 
-//		$elem.isotope({ sortBy: 'title' });
+		$elem.isotope({ sortBy: 'title' });
 	};
 }); 
--- node/public/js/c-ssIsoItem.js
+++ node/public/js/c-ssIsoItem.js
@@ -1,27 +1,29 @@
 myModule.directive('ssIsoItem', function(playerService, $timeout) {
 	
-	function extractPosition (transformString) {
-      		var re1='.*?';	// Non-greedy match on filler
-      		var re2='\\d+';	// Uninteresting: int
-      		var re3='.*?';	// Non-greedy match on filler
-      		var re4='\\d+';	// Uninteresting: int
-      		var re5='.*?';	// Non-greedy match on filler
-      		var re6='\\d+';	// Uninteresting: int
-      		var re7='.*?';	// Non-greedy match on filler
-      		var re8='\\d+';	// Uninteresting: int
-      		var re9='.*?';	// Non-greedy match on filler
-      		var re10='\\d+';	// Uninteresting: int
-      		var re11='.*?';	// Non-greedy match on filler
-      		var re12='\\d+';	// Uninteresting: int
-      		var re13='.*?';	// Non-greedy match on filler
-      		var re14='(\\d+)';	// Integer Number 1
-      		var re15='.*?';	// Non-greedy match on filler
-      		var re16='(\\d+)';	// Integer Number 2
+	function extractPosition (transformString)
+    {
+      	var re1='.*?';	// Non-greedy match on filler
+    	var re2='\\d+';	// Uninteresting: int
+      	var re3='.*?';	// Non-greedy match on filler
+      	var re4='\\d+';	// Uninteresting: int
+    	var re5='.*?';	// Non-greedy match on filler
+  		var re6='\\d+';	// Uninteresting: int
+      	var re7='.*?';	// Non-greedy match on filler
+      	var re8='\\d+';	// Uninteresting: int
+      	var re9='.*?';	// Non-greedy match on filler
+      	var re10='\\d+';	// Uninteresting: int
+      	var re11='.*?';	// Non-greedy match on filler
+      	var re12='\\d+';	// Uninteresting: int
+    	var re13='.*?';	// Non-greedy match on filler
+        var re14='(\\d+)';	// Integer Number 1
+        var re15='.*?';	// Non-greedy match on filler
+      	var re16='(\\d+)';	// Integer Number 2
 		//Regex to pull top and left values of the div out of the transform matrix
-      		var p = new RegExp(re1+re2+re3+re4+re5+re6+re7+re8+re9+re10+re11+re12+re13+re14+re15+re16,["i"]);
-      		var m = p.exec(transformString);
+      	var p = new RegExp(re1+re2+re3+re4+re5+re6+re7+re8+re9+re10+re11+re12+re13+re14+re15+re16,["i"]);
+      	var m = p.exec(transformString);
 		var position = {};
-      		if (m != null) {
+      		if (m != null)
+            {
           		var int1=m[1];
         		var int2=m[2];
 			position.left = parseInt(int1);
@@ -30,8 +32,10 @@ myModule.directive('ssIsoItem', function(playerService, $timeout) {
 		return position;
 	}
 	
-	function sortByPosition(a, b) {
-		if (Math.abs(a.domPosition.top - b.domPosition.top) < 400) {
+	function sortByPosition(a, b)
+    {
+		if (Math.abs(a.domPosition.top - b.domPosition.top) < 200)
+        {
 			return a.domPosition.left - b.domPosition.left;
 		}
 		return a.domPosition.top - b.domPosition.top;
